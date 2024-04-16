2bb981921f3b2c47ed285c95b1304159b64efc9c
josecasado <jcasadomolina@outlook.es>
2024-04-16 12:29:58 +0200

    Añadidos tests al metodo mutate

---
+++ src/test/java/org/mps/SwapMutationTest.java
@@ -0,0 +1,36 @@
+package org.mps;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.mps.mutation.SwapMutation;
+
+public class SwapMutationTest {
+    
+    @Test
+    public void  mutate_WithIndividualNull(){
+        SwapMutation mutation = new SwapMutation();
+        int[] individual = null;
+
+        assertThrows(EvolutionaryAlgorithmException.class, () -> mutation.mutate(individual));
+    }
+
+    @Test
+    public void  mutate_WithLengthLessThanZero(){
+        SwapMutation mutation = new SwapMutation();
+        int[] individual = {};
+
+        assertThrows(EvolutionaryAlgorithmException.class, () -> mutation.mutate(individual));
+    }
+
+    @Test
+    public void  mutate_WorksPropperly() throws EvolutionaryAlgorithmException{
+        SwapMutation mutation = new SwapMutation();
+        int[] individual = {1,2,3,4,5};
+
+        int[] mutatedIndividual = mutation.mutate(individual);
+
+        assertEquals(individual.length, mutatedIndividual.length);
+    }
+}
