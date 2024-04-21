d51bfbe80d2e334b1ba5a7bc59fb98de4f060345
Cano <clementecano2003@gmail.com>
2024-04-21 12:40:05 +0200

    Create TournamentSelectionTest.java

---
+++ src/test/java/org/mps/TournamentSelectionTest.java
@@ -0,0 +1,50 @@
+package org.mps;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mps.selection.TournamentSelection;
+
+public class TournamentSelectionTest{
+
+    @Test
+    @DisplayName("Si le pasamos como parámetro de entrada un número menor que 0, lanza una excepción")
+    public void constructor_WithNonCorrectParam_ThrowsException() throws EvolutionaryAlgorithmException{
+        assertThrows(EvolutionaryAlgorithmException.class, () -> new TournamentSelection(0));
+
+    }
+    
+    @Test
+    @DisplayName("Introducimos como parametro uno nulo")
+    public void select_NullParameter_ThrowsException() throws EvolutionaryAlgorithmException{
+        TournamentSelection tournamentSelection = new TournamentSelection(10);
+
+        int[] poblation = null;
+        assertThrows(EvolutionaryAlgorithmException.class, () -> tournamentSelection.select(poblation)) ;
+    }
+
+    @Test
+    @DisplayName("Introducimos como parametro uno de tamaño 0")
+    public void select_LenghtZero_ThrowsException() throws EvolutionaryAlgorithmException{
+        TournamentSelection tournamentSelection = new TournamentSelection(10);
+
+        int[] poblation = {};
+        assertThrows(EvolutionaryAlgorithmException.class, () -> tournamentSelection.select(poblation)) ;
+    }
+
+    @Test
+    @DisplayName("Introducimos datos correctos para comprobar que funciona correctamente")
+    public void select_CorrectParam_WorksProperly() throws EvolutionaryAlgorithmException{
+        TournamentSelection tournamentSelection = new TournamentSelection(10);
+
+        int[] poblation = {1,2,3,4,5,6,7};
+        int[] selected = tournamentSelection.select(poblation);
+        
+        assertEquals(7, selected.length);
+    }
+
+    
+      
+}
